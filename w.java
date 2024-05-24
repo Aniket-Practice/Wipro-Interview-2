b023fa8aba1cbf93d8fcdc55bb32db8eefe33cd5
Aniket Practice <waghmareaniket150@gmail.com>
2024-05-24 07:36:15 +0530

    Branches added

--- w.java
+++ w.java
@@ -5,5 +5,6 @@ public class w {
 
     public static void main(String[] args) {
         System.out.println("I am java class of Wipro folder");
+        System.out.println("I am Feature 2 branch");
     }
 }
